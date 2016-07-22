17df218f4992c4dddafa79fbd41ab927b39f899e
Marcin Operacz <marcin.operacz@gmail.com>
2016-07-22 20:54:15 +0200

    chore(contrib): version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nerves.Ntp.Mixfile do
   def project do
     [
       app: :nerves_ntp,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
