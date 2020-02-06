ebe1bbbe47e64f34f084cea9b2daf453df7acb1e
connor rigby <konnorrigby@gmail.com>
2020-02-06 10:34:56 -0800

    Update docs for RTC

--- lib/nerves_time/real_time_clock.ex
+++ lib/nerves_time/real_time_clock.ex
@@ -8,15 +8,15 @@ defmodule NervesTime.RealTimeClock do
 
   @doc """
   Initialize the hardware clock.
-  Called once when `ntpd` boots.
+  Called once before `ntpd` starts.
   """
   @callback init(args :: any()) :: {:ok, state} | {:error, reason :: any()}
 
   @doc """
   Retrieve the time from the hardware clock.
-  Called every time a stratum comes in from `ntpd`
+  Called once when `ntpd` before starts.
   """
-  @callback time(state) :: NaiveDateTime.t() | {:error, reason :: any()}
+  @callback time(state) :: {:ok, NaiveDateTime.t()} | {:error, reason :: any()}
 
   @doc """
   Set a time to the hardware clock.
