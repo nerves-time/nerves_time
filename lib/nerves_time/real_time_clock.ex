a3b72ba116bbe9a680d7e4b652c381f1805468bc
Frank Hunleth <fhunleth@troodon-software.com>
2020-02-16 16:32:39 -0500

    Refactor behaviour

--- lib/nerves_time/real_time_clock.ex
+++ lib/nerves_time/real_time_clock.ex
@@ -1,26 +1,40 @@
 defmodule NervesTime.RealTimeClock do
   @moduledoc """
-  Behaviour for hardware based real time clocks.
+  Behaviour for real-time clocks implementations.
   """
 
   @typedoc "Internal state of the hardware clock"
   @type state :: any()
 
   @doc """
-  Initialize the hardware clock.
-  Called once before `ntpd` starts.
+  Initialize the clock
+
+  This is called when `nerves_time` starts. If it fails, `nerves_time`
+  won't call any of the other functions.
   """
-  @callback init(args :: any()) :: {:ok, state} | {:error, reason :: any()}
+  @callback init(args :: any()) :: {:ok, state()} | {:error, reason :: any()}
 
   @doc """
-  Retrieve the time from the hardware clock.
-  Called once when `ntpd` before starts.
+  Get the time from the clock
+
+  This is called after `init/1` returns successfully to see if the
+  system clock should be updated.
+
+  If the time isn't available, the implementation should return `unavailable`.
+  `set_time/2` will be called when the time is known.
   """
-  @callback time(state) :: {:ok, NaiveDateTime.t()} | {:error, reason :: any()}
+  @callback get_time(state()) ::
+              {:ok, NaiveDateTime.t(), state()} | {:unavailable, state()}
 
   @doc """
-  Set a time to the hardware clock.
-  Called every time a stratum comes in from `ntpd`
+  Set the clock
+
+  This is called if `nerves_time` determines that the implementation is out
+  of sync with the true time and at regular intervals (usually 11 minutes) as
+  updates come in from NTP.
+
+  If the time can't be set, the implementation can either wait to be called
+  the next time or take some other action.
   """
-  @callback update(state) :: :ok | {:error, reason :: any()}
+  @callback set_time(state(), NaiveDateTime.t()) :: state()
 end
