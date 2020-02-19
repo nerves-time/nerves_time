038f0f652ec399ed18d84883c776b90359900823
Frank Hunleth <fhunleth@troodon-software.com>
2020-02-19 09:51:18 -0500

    Bump the earliest possible time to today

--- lib/nerves_time/sane_time.ex
+++ lib/nerves_time/sane_time.ex
@@ -2,7 +2,7 @@ defmodule NervesTime.SaneTime do
   # One of the ways that nerves_time determines whether a particular time is
   # possible is whether it's in a known good range.
 
-  @default_earliest_time ~N[2019-10-04 00:00:00]
+  @default_earliest_time ~N[2020-02-19 00:00:00]
   @default_latest_time %{@default_earliest_time | year: @default_earliest_time.year + 20}
 
   @moduledoc false
