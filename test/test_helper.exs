8f10c7a576420f860f81bee7500fa36992c62444
Frank Hunleth <fhunleth@troodon-software.com>
2021-05-17 23:12:52 -0400

    Always turn warnings to errors

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,4 +2,9 @@
 socket_path = Path.join(System.tmp_dir!(), "nerves_time_comm")
 File.rm(socket_path)
 
+# Always warning as errors
+if Version.match?(System.version(), "~> 1.10") do
+  Code.put_compiler_option(:warnings_as_errors, true)
+end
+
 ExUnit.start()
