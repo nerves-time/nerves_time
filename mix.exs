a1bc53a58b104e35c62646ecbf4755ae9c704223
Frank Hunleth <fhunleth@troodon-software.com>
2021-10-21 13:57:42 -0400

    Allow muontrap 1.0.0 to be used

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule NervesTime.MixProject do
 
   defp deps do
     [
-      {:muontrap, "~> 0.5"},
+      {:muontrap, "~> 1.0 or ~> 0.5"},
       {:elixir_make, "~> 0.6", runtime: false},
       {:dialyxir, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.22", only: :docs, runtime: false}
